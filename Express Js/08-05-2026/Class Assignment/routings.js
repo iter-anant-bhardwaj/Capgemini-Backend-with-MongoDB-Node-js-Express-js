d457de31ad7d54c062c552b1e26a8a27741b2e7f
iter-anant-bhardwaj <iter.anant.bhardwaj@gmail.com>
2026-05-08 12:37:19 +0530

    updated

--- Express Js/08-05-2026/Class Assignment/routings.js
+++ Express Js/08-05-2026/Class Assignment/routings.js
@@ -27,6 +27,25 @@ router.delete("/users/:id", (req, res) => {
   res.send(`DELETE user ${req.params.id}`);
 });
 
+// Error handling Middleware
+// router.use((req, res)=>{
+//   if(req.method !== "GET" || req.method !== "PUT" || req.method !== "POST" || req.method === "DELETE" || req.method !== "PATCH"){
+//     return res.status(405).send("Method not allowed");
+//   }
+//   res.status(404).send("Page not found");
+// })
+
+
+
+// Handle unsupported methods
+router.all("/users/:id", (req, res) => {
+  res.status(405).send("Method Not Allowed");
+});
+
+router.use((req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
 
 
 module.exports = router;
